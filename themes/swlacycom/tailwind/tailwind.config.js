4bc94dbc8136f32fafbc72f0a97b0b6a35974d43
Sid Lacy <contact@swlacy.com>
2025-01-10 19:28:12 -0800

    minor style tweaks

--- themes/swlacycom/tailwind/tailwind.config.js
+++ themes/swlacycom/tailwind/tailwind.config.js
@@ -6,9 +6,11 @@ module.exports = {
         extend: {
             colors: {
                 "light-bg": "#ffffff",
+                "light-callout": "#ececec",
                 "light-text": "#3b3b3b",
                 "light-accent": "#60acf7",
                 "dark-bg": "#1b1b1f",
+                "dark-callout": "#313135",
                 "dark-text": "#ffffff",
                 "dark-accent": "#a9b3fb",
             }
